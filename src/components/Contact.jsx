a0c5b945950e2ec001c359873bb3a33f160e0de7
facund1n <facus16s@gmail.com>
2023-04-07 15:10:15 -0300

    br delete, pb-2 add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 const Contact = () => {
   return (
-    <>
+    <div className="pb-2">
       <h3 className="font-green-light">Contact:</h3>
       <div>E-mail</div>
       <a href="mailto:facus16s@gmail.com">facus16s@gmail.com</a>
@@ -10,8 +10,7 @@ const Contact = () => {
       <div>+54 9 3813478874</div>
       <div>CUIT/CUIL</div>
       <div>23 34186604 9</div>
-      <br />
-    </>
+    </div>
   );
 };
 
